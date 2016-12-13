de279b3ae765179bd8ad6a5b4c26d35ceb89a776
宮澤賢生 <yochio@yochio.local>
2016-12-13 15:24:09 +0900

    Initial Commit

---
+++ CalcApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CalcApp
+//
+//  Created by 宮澤賢生 on 2016/12/13.
+//  Copyright © 2016年 宮澤賢生. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
